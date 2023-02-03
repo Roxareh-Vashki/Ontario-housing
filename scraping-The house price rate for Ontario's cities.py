3768110320316dde2ea3542e635cc8ca3c2a4af9
Roxareh-Vashki <101058050+Roxareh-Vashki@users.noreply.github.com>
2023-02-03 18:36:07 -0500

    Update scraping-The house price rate for Ontario's cities.py

--- scraping-The house price rate for Ontario's cities.py
+++ scraping-The house price rate for Ontario's cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[6]:
 
 
 # Importing the required libraries
@@ -15,26 +15,15 @@ from typing import List
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
-# url = "https://ajax.listing.ca/real-estate-price-history.htm"
-# dfs = pd.read_html(url, header = 0 )
-# df1 = dfs[1:12]
-# df1
 
-
-# In[ ]:
-
-
-
-
-
-# In[4]:
+# In[7]:
 
 
 def get_data(url: str) -> pd.DataFrame:
     # GET THE HEADERS
     url = "https://ajax.listing.ca/real-estate-price-history.htm"
     dfs = pd.read_html(url, header = 0 )
-    df1 = dfs[1:12]
+    df1 = dfs[7:12]
     
     #list of dataframes to data a dataframe
     df2 = pd.concat(df1)
@@ -48,19 +37,12 @@ url = "https://ajax.listing.ca/real-estate-price-history.htm"
 get_data(url)
 
 
-# In[5]:
-
-
-#gol.insert(0, 'year', '2022')
-#gol['year'] = pd.Series([2022 for x in range(len(gol.index))])
-#gol1 = gol[gol.index % 12 != 0]
-
-#def get_year(dataframe):
+# In[8]:
 
 
 def add_year(dataframe):
     dataframe.insert(0,'year', '')
-    for i in range(0,120):
+    for i in range(0,60):
         x = 2022 - i*(1/12)
         y = math.ceil(x)
         dataframe.iloc[i, dataframe.columns.get_loc('year')] = y
@@ -72,18 +54,7 @@ table = get_data(url)
 Ajax_Sales=add_year(table)
 
 
-
-    
-#     #new =gol.iloc[0::11, :]
-#     y = i + 2011
-#     gol['year'] = pd.Series([y for x in range(1,12)])
-Ajax_Sales.head(44)
-
-
-# In[4]:
-
-
-xx.tail(15)
+Ajax_Sales.head(60)
 
 
 # In[9]:
@@ -92,12 +63,12 @@ xx.tail(15)
 Ajax_Sales.to_csv(r"C:\Users\rokhs\OneDrive\Documents\projects\house\csv from python\Ajax_Sales.csv")
 
 
-# In[12]:
+# In[10]:
 
 
 def get_data(url: str) -> pd.DataFrame:
     # GET THE HEADERS
-    url = "https://ajax.listing.ca/real-estate-price-history.htm"
+    url = "https://brampton.listing.ca/real-estate-price-history.htm"
     dfs = pd.read_html(url, header = 0 )
     df1 = dfs[1:12]
     
@@ -128,12 +99,12 @@ Brampton_Sales=add_year(table)
 Brampton_Sales.to_csv(r"C:\Users\rokhs\OneDrive\Documents\projects\house\csv from python\Brampton_Sales.csv")
 
 
-# In[14]:
+# In[11]:
 
 
 def get_data(url: str) -> pd.DataFrame:
     # GET THE HEADERS
-    url = "https://ajax.listing.ca/real-estate-price-history.htm"
+    url = "https://richmond-hill.listing.ca/real-estate-price-history.htm"
     dfs = pd.read_html(url, header = 0 )
     df1 = dfs[1:12]
     
@@ -164,36 +135,192 @@ Richmondhill_Sales=add_year(table)
 Richmondhill_Sales.to_csv(r"C:\Users\rokhs\OneDrive\Documents\projects\house\csv from python\Richmondhill_Sales.csv")
 
 
-# In[5]:
+# In[12]:
 
 
-# def data_frame(year):
-#     df_year=df[2022-year]
-#     return(df_year)
+def get_data(url: str) -> pd.DataFrame:
+    # GET THE HEADERS
+    url = "https://aurora.listing.ca/real-estate-price-history.htm"
+    dfs = pd.read_html(url, header = 0 )
+    df1 = dfs[1:12]
+    
+    #list of dataframes to data a dataframe
+    df2 = pd.concat(df1)
+    df = df2.reset_index(drop = True)
+    return df
+    
 
-# newdf= pd.DataFrame()
-# for i in range(2012,2022):
-#     ds= data_frame(i)
-#     dr = newdf.append(ds)
-
-# print(dr)
-# dr.tail()
-
-
-# In[6]:
-
-
-# year = 2022
-# print(year , dataframe(year))
+    logging.info(f"Getting data for: {k}; Starting Row: {v}")
+    
+url = "https://aurora.listing.ca/real-estate-price-history.htm" 
+get_data(url)
 
 
-# In[7]:
+def add_year(dataframe):
+    dataframe.insert(0,'year', '')
+    for i in range(0,120):
+        x = 2022 - i*(1/12)
+        y = math.ceil(x)
+        dataframe.iloc[i, dataframe.columns.get_loc('year')] = y
+    return dataframe
+    
+table = get_data(url)
 
 
-#print(f'Total tables: {len(url)}')
+aurora_Sales=add_year(table)
+aurora_Sales.to_csv(r"C:\Users\rokhs\OneDrive\Documents\projects\house\csv from python\aurora_Sales.csv")
 
 
-# In[ ]:
+# In[13]:
+
+
+def get_data(url: str) -> pd.DataFrame:
+    # GET THE HEADERS
+    url = "https://markham.listing.ca/real-estate-price-history.htm"
+    dfs = pd.read_html(url, header = 0 )
+    df1 = dfs[1:12]
+    
+    #list of dataframes to data a dataframe
+    df2 = pd.concat(df1)
+    df = df2.reset_index(drop = True)
+    return df
+    
+
+    logging.info(f"Getting data for: {k}; Starting Row: {v}")
+    
+url = "https://markham.listing.ca/real-estate-price-history.htm" 
+get_data(url)
+
+
+def add_year(dataframe):
+    dataframe.insert(0,'year', '')
+    for i in range(0,120):
+        x = 2022 - i*(1/12)
+        y = math.ceil(x)
+        dataframe.iloc[i, dataframe.columns.get_loc('year')] = y
+    return dataframe
+    
+table = get_data(url)
+
+
+markham_Sales=add_year(table)
+markham_Sales.to_csv(r"C:\Users\rokhs\OneDrive\Documents\projects\house\csv from python\markham_Sales.csv")
+
+
+# In[14]:
+
+
+def get_data(url: str) -> pd.DataFrame:
+    # GET THE HEADERS
+    url = "https://mississauga.listing.ca/real-estate-price-history.htm"
+    dfs = pd.read_html(url, header = 0 )
+    df1 = dfs[1:12]
+    
+    #list of dataframes to data a dataframe
+    df2 = pd.concat(df1)
+    df = df2.reset_index(drop = True)
+    return df
+    
+
+    logging.info(f"Getting data for: {k}; Starting Row: {v}")
+    
+url = "https://mississauga.listing.ca/real-estate-price-history.htm" 
+get_data(url)
+
+
+def add_year(dataframe):
+    dataframe.insert(0,'year', '')
+    for i in range(0,120):
+        x = 2022 - i*(1/12)
+        y = math.ceil(x)
+        dataframe.iloc[i, dataframe.columns.get_loc('year')] = y
+    return dataframe
+    
+table = get_data(url)
+
+
+mississauga_Sales=add_year(table)
+mississauga_Sales.to_csv(r"C:\Users\rokhs\OneDrive\Documents\projects\house\csv from python\mississauga_Sales.csv")
+
+
+# In[15]:
+
+
+def get_data(url: str) -> pd.DataFrame:
+    # GET THE HEADERS
+    url = "https://newmarket.listing.ca/real-estate-price-history.htm"
+    dfs = pd.read_html(url, header = 0 )
+    df1 = dfs[1:12]
+    
+    #list of dataframes to data a dataframe
+    df2 = pd.concat(df1)
+    df = df2.reset_index(drop = True)
+    return df
+    
+
+    logging.info(f"Getting data for: {k}; Starting Row: {v}")
+    
+url = "https://newmarket.listing.ca/real-estate-price-history.htm" 
+get_data(url)
+
+
+def add_year(dataframe):
+    dataframe.insert(0,'year', '')
+    for i in range(0,120):
+        x = 2022 - i*(1/12)
+        y = math.ceil(x)
+        dataframe.iloc[i, dataframe.columns.get_loc('year')] = y
+    return dataframe
+    
+table = get_data(url)
+
+
+newmarket_Sales=add_year(table)
+newmarket_Sales.to_csv(r"C:\Users\rokhs\OneDrive\Documents\projects\house\csv from python\newmarket_Sales.csv")
+
+
+# In[16]:
+
+
+def get_data(url: str) -> pd.DataFrame:
+    # GET THE HEADERS
+    url = "https://vaughan.listing.ca/real-estate-price-history.htm"
+    dfs = pd.read_html(url, header = 0 )
+    df1 = dfs[1:12]
+    
+    #list of dataframes to data a dataframe
+    df2 = pd.concat(df1)
+    df = df2.reset_index(drop = True)
+    return df
+    
+
+    logging.info(f"Getting data for: {k}; Starting Row: {v}")
+    
+url = "https://vaughan.listing.ca/real-estate-price-history.htm" 
+get_data(url)
+
+
+def add_year(dataframe):
+    dataframe.insert(0,'year', '')
+    for i in range(0,120):
+        x = 2022 - i*(1/12)
+        y = math.ceil(x)
+        dataframe.iloc[i, dataframe.columns.get_loc('year')] = y
+    return dataframe
+    
+table = get_data(url)
+
+
+vaughan_Sales=add_year(table)
+vaughan_Sales.to_csv(r"C:\Users\rokhs\OneDrive\Documents\projects\house\csv from python\vaughan_Sales.csv")
+
+
+
+
+
+
+
+
 
 
 
